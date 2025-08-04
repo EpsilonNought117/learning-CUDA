e1b38bd6005f96f8721d24f10eca041d8363fae1
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2025-08-04 17:34:05 +0530

    updated softmax code

--- softmax.cu
+++ softmax.cu
@@ -28,8 +28,14 @@
 #define MIN_LOGIT		-50.0f
 #define MAX_LOGIT		50.0f
 #define COMBINED_MAX	((1UL << 30) - 1)
-#define THREADS_PER_BLK	512UL
 #define ELEM_COUNT      (10UL * 1000 * 1000UL)
+
+// Tested for RTX 4060 Laptop GPU (AD107)
+
+#define THREADS_PER_BLK	512UL
+#define SM_COUNT		24UL
+#define BLK_PER_SM		3UL
+#define TOTAL_BLKS		SM_COUNT * BLK_PER_SM
 
 // Utility
 static inline float getRandLogitInRange(float maxVal, float minVal)
@@ -141,37 +147,34 @@ __global__ void normalize_d(float* input, unsigned int size, float sumVal)
 		input[i] /= sumVal;
 }
 
-int main(void) {
-	const unsigned int total_elements = ELEM_COUNT;
-	const unsigned int threads_per_block = THREADS_PER_BLK;
-	const unsigned int num_blocks = (total_elements + threads_per_block - 1) / threads_per_block;
-
+int main(void)
+{
 	// Allocate host memory
-	float* h_input = (float*)malloc(sizeof(float) * total_elements);
-	float* h_cpu_output = (float*)malloc(sizeof(float) * total_elements);
-	float* h_gpu_output = (float*)malloc(sizeof(float) * total_elements);
-	float* h_max_reduced = (float*)malloc(sizeof(float) * num_blocks);
-	float* h_sum_reduced = (float*)malloc(sizeof(float) * num_blocks);
+	float* h_input = (float*)malloc(sizeof(float) * ELEM_COUNT);
+	float* h_cpu_output = (float*)malloc(sizeof(float) * ELEM_COUNT);
+	float* h_gpu_output = (float*)malloc(sizeof(float) * ELEM_COUNT);
+	float* h_max_reduced = (float*)malloc(sizeof(float) * TOTAL_BLKS);
+	float* h_sum_reduced = (float*)malloc(sizeof(float) * TOTAL_BLKS);
 	HOST_CHECK(h_input && h_cpu_output && h_gpu_output && h_max_reduced && h_sum_reduced, "Host malloc failed");
 
 	// Initialize input
-	for (unsigned int i = 0; i < total_elements; i++)
+	for (unsigned int i = 0; i < ELEM_COUNT; i++)
 		h_input[i] = getRandLogitInRange(MAX_LOGIT, MIN_LOGIT);
 
-	memcpy(h_cpu_output, h_input, sizeof(float) * total_elements);
+	memcpy(h_cpu_output, h_input, sizeof(float) * ELEM_COUNT);
 
 	// CPU softmax
-	float max_cpu = findMax_h(h_cpu_output, total_elements);
-	subMax_h(h_cpu_output, total_elements, max_cpu);
-	float sum_cpu = expAndSum_h(h_cpu_output, total_elements);
-	normalize_h(h_cpu_output, total_elements, sum_cpu);
+	float max_cpu = findMax_h(h_cpu_output, ELEM_COUNT);
+	subMax_h(h_cpu_output, ELEM_COUNT, max_cpu);
+	float sum_cpu = expAndSum_h(h_cpu_output, ELEM_COUNT);
+	normalize_h(h_cpu_output, ELEM_COUNT, sum_cpu);
 
 	// GPU softmax
 	float* d_input, * d_max, * d_sum;
-	CUDA_CHECK(cudaMalloc(&d_input, sizeof(float) * total_elements));
-	CUDA_CHECK(cudaMalloc(&d_max, sizeof(float) * num_blocks));
-	CUDA_CHECK(cudaMalloc(&d_sum, sizeof(float) * num_blocks));
-	CUDA_CHECK(cudaMemcpy(d_input, h_input, sizeof(float) * total_elements, cudaMemcpyHostToDevice));
+	CUDA_CHECK(cudaMalloc(&d_input, sizeof(float) * ELEM_COUNT));
+	CUDA_CHECK(cudaMalloc(&d_max, sizeof(float) * TOTAL_BLKS));
+	CUDA_CHECK(cudaMalloc(&d_sum, sizeof(float) * TOTAL_BLKS));
+	CUDA_CHECK(cudaMemcpy(d_input, h_input, sizeof(float) * ELEM_COUNT, cudaMemcpyHostToDevice));
 
 	cudaEvent_t start, stop;
 	float elapsed_ms;
@@ -179,28 +182,28 @@ int main(void) {
 	CUDA_CHECK(cudaEventCreate(&stop));
 	CUDA_CHECK(cudaEventRecord(start));
 
-	findMax_d << <num_blocks, threads_per_block >> > (d_input, total_elements, d_max);
+	findMax_d << <TOTAL_BLKS, THREADS_PER_BLK >> > (d_input, ELEM_COUNT, d_max);
 	CUDA_CHECK(cudaGetLastError());
-	CUDA_CHECK(cudaMemcpy(h_max_reduced, d_max, sizeof(float) * num_blocks, cudaMemcpyDeviceToHost));
+	CUDA_CHECK(cudaMemcpy(h_max_reduced, d_max, sizeof(float) * TOTAL_BLKS, cudaMemcpyDeviceToHost));
 
 	float max_gpu = -INFINITY;
-	for (unsigned int i = 0; i < num_blocks; ++i)
+	for (unsigned int i = 0; i < TOTAL_BLKS; ++i)
 		if (h_max_reduced[i] > max_gpu) max_gpu = h_max_reduced[i];
 
-	subAndExp_d << <num_blocks, threads_per_block >> > (d_input, total_elements, max_gpu);
+	subAndExp_d << <TOTAL_BLKS, THREADS_PER_BLK >> > (d_input, ELEM_COUNT, max_gpu);
 	CUDA_CHECK(cudaGetLastError());
 
-	sum_d << <num_blocks, threads_per_block >> > (d_input, total_elements, d_sum);
+	sum_d << <TOTAL_BLKS, THREADS_PER_BLK >> > (d_input, ELEM_COUNT, d_sum);
 	CUDA_CHECK(cudaGetLastError());
-	CUDA_CHECK(cudaMemcpy(h_sum_reduced, d_sum, sizeof(float) * num_blocks, cudaMemcpyDeviceToHost));
+	CUDA_CHECK(cudaMemcpy(h_sum_reduced, d_sum, sizeof(float) * TOTAL_BLKS, cudaMemcpyDeviceToHost));
 
 	float sum_gpu = 0.0f;
-	for (unsigned int i = 0; i < num_blocks; ++i)
+	for (unsigned int i = 0; i < TOTAL_BLKS; ++i)
 		sum_gpu += h_sum_reduced[i];
 
-	normalize_d << <num_blocks, threads_per_block >> > (d_input, total_elements, sum_gpu);
+	normalize_d << <TOTAL_BLKS, THREADS_PER_BLK >> > (d_input, ELEM_COUNT, sum_gpu);
 	CUDA_CHECK(cudaGetLastError());
-	CUDA_CHECK(cudaMemcpy(h_gpu_output, d_input, sizeof(float) * total_elements, cudaMemcpyDeviceToHost));
+	CUDA_CHECK(cudaMemcpy(h_gpu_output, d_input, sizeof(float) * ELEM_COUNT, cudaMemcpyDeviceToHost));
 
 	CUDA_CHECK(cudaEventRecord(stop));
 	CUDA_CHECK(cudaEventSynchronize(stop));
@@ -208,11 +211,15 @@ int main(void) {
 	printf("Softmax completed. CUDA time: %.3f ms\n", elapsed_ms);
 
 	// Write to CSV
+
 	FILE* file = fopen("softmax_outputs.csv", "w");
 	HOST_CHECK(file != NULL, "Failed to open CSV file");
+
 	fprintf(file, "Sno.,GPU Output,CPU Output\n");
-	for (unsigned int i = 0; i < total_elements; i++)
+
+	for (unsigned int i = 0; i < ELEM_COUNT; i++)
 		fprintf(file, "%u,%.15g,%.15g\n", i, h_gpu_output[i], h_cpu_output[i]);
+	
 	fclose(file);
 	printf("Output written to softmax_outputs.csv\n");
 
